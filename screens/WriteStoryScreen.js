14444f72fcf493992157b1506505dc0815e057d8
casualGamer-dev <60314853+casualGamer-dev@users.noreply.github.com>
2020-11-02 22:42:01 +0530

    Update WriteStoryScreen.js

--- screens/WriteStoryScreen.js
+++ screens/WriteStoryScreen.js
@@ -1,41 +1,23 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text,TouchableOpacity, View,Image } from 'react-native';
+import React { Component } from 'react';
+import { StyleSheet, Text,TextInput,Image, View ,TouchableOpacity,KeyboardAvoidingView,Alert} from 'react-native';
+import firebase from 'firebase';
+import db from  '../config'
+export default class ReadStoryScreen extends Component{
 
-export default class WriteStoryScreen extends Component{
-    constructor(){
-        super();
-        this.setState={
-            title:"",
-            story:"",
-            buttonState:'normal',
-            bookWritten:false,
-            story:""
-        }
-    }
-    handlestoryState =async(data) =>{
-        const {buttonState}=this.state
-        if(buttonState==="pressed"){
-         this.setState({
-             bookWritten:true,
-             story:"data"
-         })
-        }
-    }
-    handleStoryTitle = async(data)=>{
-        const buttonState= this.state
-        if (buttonState==="pressed"){
-        this.setState({
-            title:"data"
-        })
-        }
-    }
-    
+
     render(){
         return(
         <View style={{flex:5,alignItems:"center"}}>
-        <Image source={require('../assets/write.png')} style={{width:400,height:400}}></Image>
-          
-         </View>
+        <Image source={require('../assets/write.png')} style={{width:1500,height:150}}></Image>
+        <Text style={{textAlign:"center",fontSize:60}}>
+         <TextInput >
+           <Text> story title</Text>
+         </TextInput>
+         <TextInput>
+           <Text> author</Text>
+         </TextInput>
+        </Text>
+    </View>
     )
     }
 }
